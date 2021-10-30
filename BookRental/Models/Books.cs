55b3b4efcd82ecf9cce24eb1bcc4e6d95331cdaa
John Kehinde <kehindejohn018@gmail.com>
2021-10-30 10:30:01 +0100

    Added a book entity

---
+++ BookRental/Models/Books.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookRental.Models
+{
+    public class Books
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}

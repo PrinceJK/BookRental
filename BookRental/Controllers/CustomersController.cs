7af90d676303efc0f08ec8260dd7d0970b641ee5
John Kehinde <kehindejohn018@gmail.com>
2021-11-10 18:10:34 +0100

    Added datatable for the customer CRUD operations

--- BookRental/Controllers/CustomersController.cs
+++ BookRental/Controllers/CustomersController.cs
@@ -32,10 +32,8 @@ namespace BookRental.Controllers
         }
         public ViewResult Index()
         {
-            var customers = _context.Customers.Include(
-                c => c.MembershipType).ToList();
 
-            return View(customers);
+            return View();
         }
 
         public ActionResult Details(int id)
